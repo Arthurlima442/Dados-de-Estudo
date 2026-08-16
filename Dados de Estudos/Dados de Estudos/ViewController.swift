0b2e185a1332a3667c82ddf1c307e8df9411369a
Arthur Lima <arthurlima442@gmail.com>
2026-08-16 13:29:04 -0300

    config inicial

---
+++ Dados de Estudos/Dados de Estudos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dados de Estudos
+//
+//  Created by Arthur Lima on 16/08/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
